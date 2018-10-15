ee4f10279360e03d21b0fede2919cfc47e1c48c7
Geoff Small <gsmall@superheatfgh.com>
2018-10-15 09:13:19 -0400

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,44 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) =>{
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connect to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // // deleteMany
+    // db.collection('Todos').deleteMany({
+    //     text: 'Eat lunch'
+    // }).then((result)=>{
+    //     console.log(result);
+    // })
+
+    // // deleteOne
+    // db.collection('Todos').deleteOne({
+    //     text: 'Eat lunch'
+    // }).then((result)=>{
+    //     console.log(result);
+    // })
+
+    // findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({
+    //     completed: false
+    // }).then(result => {
+    //     console.log(result);
+    // })
+
+    db.collection('Users').deleteMany({
+        name: 'Geoff Small'
+    }).then(result => {
+        console.log(result);
+
+    })
+    //.deleteOne({  
+    //     _id: new ObjectID("5bbf483574b35a6144d7e0e6")
+    // }).then(result => {
+    //     console.log(result);
+    // })
+
+    //client.close();
+});
