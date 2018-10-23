90de0792e7f4d43aaeacc36584aca0286dcfd096
Geoff Small <gsmall@superheatfgh.com>
2018-10-23 09:10:31 -0400

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -28,10 +30,32 @@ app.get('/todos', (req, res)=>{
     },(e)=>{
         res.status(400).send(e);
     });
-})
+});
 
-app.listen(3000, ()=>{
-    console.log('Started on port 3000');
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    //res.send(req.params);
+
+    if(!ObjectID.isValid(id)){
+        console.log('ID not valid');
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }else{
+            
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+    
+});
+
+app.listen(port, ()=>{
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
